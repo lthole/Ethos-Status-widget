8b5cfd28062c7e3ce91cd43f25c2f606cbfc9364
Lothar Thole <lothar.thole@gmail.com>
2025-07-11 17:03:39 +0930

    Add cs.lua

---
+++ status3/i18n/cs.lua
@@ -0,0 +1,18 @@
+return {
+  scriptName = "Status3",
+  source = "Zdroj",
+  state1def = "Stav 1",
+  state2def = "Stav 2",
+  state3def = "Stav 3",
+  state1Text = "Text Stavu 1",
+  state2Text = "Text Stavu 2",
+  state3Text = "Text Stavu 3",
+  state1Threshold = "Prahová hodnota Stavu 1",
+  state2Threshold = "Prahová hodnota Stavu 2",
+  fontSize = "Velikost písma",
+  color1 = "Barva pozadí Stavu 1",
+  color2 = "Barva pozadí Stavu 2",
+  color3 = "Barva pozadí Stavu 3",
+  debugMode = "Zobrazit hodnoty pro ladění",
+  threshold = "Prahová hodnota"
+}
